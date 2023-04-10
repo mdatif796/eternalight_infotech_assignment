7059ce73d00b56a5add80342d1e821b9cc31fa2b
mdatif796 <mdatif796@gmail.com>
2023-04-10 22:49:07 +0530

    completed project

---
+++ client/src/components/App.js
@@ -0,0 +1,48 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Navbar from "./Navbar";
+import Loader from "./Loader";
+import { useAuth } from "../hooks";
+import LogIn from "./LogIn";
+import SignUp from "./SignUp";
+import Home from "./Home";
+
+const PrivateRoute = ({ children }) => {
+  const auth = useAuth();
+  return auth.user ? children : <Navigate to="/login" />;
+};
+
+function App() {
+  const auth = useAuth();
+
+  if (auth.loading) {
+    return <Loader />;
+  }
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="*" element={<h1>Route not found!!</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import App from "./components/App";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AuthProvider } from "./providers/authProvider";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+    <ToastContainer autoClose="1500" position="top-left" />
+  </React.StrictMode>
+);
